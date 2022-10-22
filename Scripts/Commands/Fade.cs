cbbbd12655c32d5a3c1874b18c51f0d1b1090bf2
miyamoto-tatsuki <i.love.wolf090@gmail.com>
2022-10-22 16:44:25 +0900

    フェードのデフォルトを1fに変更

--- Scripts/Commands/Fade.cs
+++ Scripts/Commands/Fade.cs
@@ -7,24 +7,40 @@ using Naninovel.UI;
 [CommandAlias("fadeOut")]
 public class FadeOut : Command
 {
+    public DecimalParameter FadeTime;
+
     public override async UniTask ExecuteAsync (AsyncToken asyncToken = default)
     {
         Debug.Log("暗転");
 
+        float time = 1f;
+        if(Assigned(FadeTime))
+        {
+            time = FadeTime;
+        }
+
         var blackoutUI = Engine.GetService<IUIManager>().GetUI<IFadeUI>();
-        await blackoutUI.Show(true);
+        await blackoutUI.Show(true, time);
     }
 }
 
 [CommandAlias("fadeIn")]
 public class FadeIn : Command
 {
+    public DecimalParameter FadeTime;
+
     public override async UniTask ExecuteAsync (AsyncToken asyncToken = default)
     {
         Debug.Log("暗転");
 
+        float time = 1f;
+        if(Assigned(FadeTime))
+        {
+            time = FadeTime;
+        }
+
         var blackoutUI = Engine.GetService<IUIManager>().GetUI<IFadeUI>();
-        await blackoutUI.Show(false);
+        await blackoutUI.Show(false, time);
         blackoutUI.Hide();
     }
 }
