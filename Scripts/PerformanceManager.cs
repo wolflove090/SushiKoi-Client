12a1aef93baf829dff1586169b09ec074fd870ef
miyamoto-tatsuki <i.love.wolf090@gmail.com>
2023-03-03 19:50:05 +0900

    メモリ取得処理作成

--- Scripts/PerformanceManager.cs
+++ Scripts/PerformanceManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Profiling;
 using TMPro;
 
 public class PerformanceManager : MonoBehaviour
@@ -35,15 +36,49 @@ public class PerformanceManager : MonoBehaviour
     void Update()
     {
         this._FrameCount++;
-        this._Time += Time.unscaledDeltaTime;
+        this._Time += Time.deltaTime;
 
         if(this._Time >= 0.5f)
         {
+            // FPS更新
             var fps = Mathf.Floor(this._FrameCount / this._Time);
             this._FpsValue.text = fps.ToString();
 
             this._Time = 0;
             this._FrameCount = 0;
+
+            // メモリ使用量更新
+            // UnityのProfiler経由なので正確ではないことに注意
+            this._MemoryValue.text = ConvertMemorySizeToBestUnit(Profiler.GetTotalAllocatedMemoryLong());
         }
+    }
+
+    string ConvertMemorySizeToBestUnit(long memory)
+    {
+        int count = 0;
+        while(memory > 1024)
+        {
+            memory = memory / 1024;
+            count++;
+        }
+
+        string unit = "";
+        switch(count)
+        {
+            case 0:
+                unit = "B";
+                break;
+            case 1:
+                unit = "KB";
+                break;
+            case 2:
+                unit = "MB";
+                break;
+            case 3:
+                unit = "GB";
+                break;
+        }
+
+        return $"{memory}{unit}";
     }
 }
